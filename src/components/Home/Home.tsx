6889444783e99949a0ce0e5c9454a985c13b9659
mr-kasper <rachidwork5050@gmail.com>
2025-05-07 20:29:11 +0100

    Add Services section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import About from './About/About';
 import Hero from './Hero/Hero';
+import Services from './Services/Services';
 
 const Home = () => {
   return (
     <main className="overflow-hidden">
       <Hero />
       <About />
+      <Services />
     </main>
   );
 };
---
+++ src/components/Home/Services/Services.tsx
@@ -0,0 +1,19 @@
+import SectionHeading from '@/components/Helper/SectionHeading';
+import { servicesData } from '@/Data/data';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+  return (
+    <div className="py-16 bg-[#0F0715]">
+      <SectionHeading>Services</SectionHeading>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-[80%] mx-auto items-center mt-20">
+        {servicesData.map((service) => (
+          <div key={service.id}>
+            <ServiceCard service={service} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Services;
