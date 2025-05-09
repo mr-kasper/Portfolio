ea6b88d230ed4276935a7517f5bc761c8bceb363
mr-kasper <rachidwork5050@gmail.com>
2025-05-09 12:21:18 +0100

    Add animated cursor and custom scrollbar styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ResponsiveNav from '@/components/Home/Navbar/ResponsiveNav';
 import Footer from '@/components/Home/Footer/Footer';
 import { Toaster } from 'react-hot-toast';
 import ScrollToTop from '@/components/Helper/ScrollToTop';
+import AnimatedCursor from 'react-animated-cursor';
 
 const font = Sora({
   variable: '--font-sora',
@@ -23,8 +24,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth custom-scrollbar">
       <body className={`${font.variable} antialiased`}>
+        <div className="hidden md:block">
+          <AnimatedCursor
+            innerSize={8}
+            outerSize={30}
+            outerAlpha={0}
+            innerScale={2}
+            outerScale={2}
+            innerStyle={{
+              backgroundColor: 'white',
+            }}
+            outerStyle={{
+              border: '2px solid #fff',
+            }}
+          />
+        </div>
         <Toaster position="top-center" reverseOrder={false} />
         <ResponsiveNav />
         {children}
